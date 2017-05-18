5ecea6cfe2304cc7bd3cc0b29a02e66c78121dcc
Jonathan Duran <headjd@gmail.com>
2017-05-17 20:02:23 -0400

    Rutas para Alimento+Bebida+Material

--- routes/web.php
+++ routes/web.php
@@ -31,17 +31,35 @@ Route::group(['prefix' => 'admin','middleware' => 'auth'], function () {
     Route::resource('user', 'UserController');
     Route::resource('productService', 'ProductServiceController');
     Route::resource('presupuesto', 'PresupuestoController');
+
     Route::get('evento/buscarNombreEvento/{value}','EventoController@buscarNombreEvento');
     Route::get('evento/buscarCodigoEvento/{value}','EventoController@buscarCodigoEvento');
     Route::get('evento/autoIncrementoEvento','EventoController@autoIncrementoEvento');
     Route::resource('evento', 'EventoController');
+
     Route::get('salon/buscarNombreSalon/{value}','SalonController@buscarNombreSalon');
     Route::get('salon/buscarCodigoSalon/{value}','SalonController@buscarCodigoSalon');
     Route::get('salon/autoIncrementoSalon','SalonController@autoIncrementoSalon');
     Route::resource('salon', 'SalonController');
+
     Route::get('montaje/buscarNombreMontaje/{value}','MontajeController@buscarNombreMontaje');
     Route::get('montaje/buscarCodigoMontaje/{value}','MontajeController@buscarCodigoMontaje');
     Route::get('montaje/autoIncrementoMontaje','MontajeController@autoIncrementoMontaje');
     Route::resource('montaje', 'MontajeController');
+
+    Route::get('alimento/buscarNombreAlimento/{value}','AlimentoController@buscarNombreAlimento');
+    Route::get('alimento/buscarCodigoAlimento/{value}','AlimentoController@buscarCodigoAlimento');
+    Route::get('alimento/autoIncrementoAlimento','AlimentoController@autoIncrementoAlimento');
+    Route::resource('alimento', 'AlimentoController');
+
+    Route::get('babida/buscarNombreBebida/{value}','BebidaController@buscarNombreBebida');
+    Route::get('babida/buscarCodigoBebida/{value}','BebidaController@buscarCodigoBebida');
+    Route::get('babida/autoIncrementoBebida','BebidaController@autoIncrementoBebida');
+    Route::resource('babida', 'BebidaController');
+
+    Route::get('material/buscarNombreMaterial/{value}','MaterialController@buscarNombreMaterial');
+    Route::get('material/buscarCodigoMaterial/{value}','MaterialController@buscarCodigoMaterial');
+    Route::get('material/autoIncrementoMaterial','MaterialController@autoIncrementoMaterial');
+    Route::resource('material', 'MaterialController');
 
 });
